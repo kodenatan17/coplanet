4fa84d99ed2e23a86d1377a064f57522d7696cff
ferry-wibisono <ferry.wibisono@salt.co.id>
2023-07-02 19:16:22 +0700

    voucher detail finishing

--- services/player.ts
+++ services/player.ts
@@ -1,15 +1,19 @@
 import axios from "axios";
+const API_URL = process.env.NEXT_PUBLIC_API;
+const API_VERSION = 'api/v1';
 
 export async function getFeaturedMenu() {
-    const API_URL = process.env.NEXT_PUBLIC_API;
-    const API_VERSION = 'api/v1';
     const URL = 'players/landing-page';
-    
+
     const response = await axios.get(`${API_URL}/${API_VERSION}/${URL}`);
     const axiosResponse = response.data;
     return axiosResponse.data;
 }
 
-export async function getDetailVoucher() {
- return null;
+export async function getDetailVoucher(id: any) {
+    const URL = `players/${id}/detail`;
+
+    const response = await axios.get(`${API_URL}/${API_VERSION}/${URL}`);
+    const axiosResponse = response.data;
+    return axiosResponse.data;
 }
