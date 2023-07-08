3eedd237e1f2e2f4bc3f23d79c96ba43ab9707cc
ferry-wibisono <ferry.wibisono@salt.co.id>
2023-07-08 11:38:37 +0700

    integration on signUp photo URL && local storage
    - added on local storage env
    - init service for auth register API

--- src/pages/sign-up-photo.tsx
+++ src/pages/sign-up-photo.tsx
@@ -1,22 +1,49 @@
 import Image from "next/image";
 import React, { useCallback, useEffect, useState } from "react";
+import { setSignUp } from "../../services/auth";
 import { getMenuCategory } from "../../services/player";
 
 export default function SignUpPhoto() {
   const [categories, setCategories] = useState([]);
   const [favorite, setFavorite] = useState("");
+  const [image, setImage] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+  const [localForm, setLocalForm] = useState({
+    name: "",
+    email: "",
+  });
+
   const getMenuCategoryAPI = useCallback(async () => {
     const data = await getMenuCategory();
-    console.log("data : ", data);
     setCategories(data);
     setFavorite(data[0]._id);
   }, []);
   useEffect(() => {
     getMenuCategoryAPI();
-  });
+  }, []);
+  useEffect(() => {
+    const getLocalForm = localStorage.getItem("user-form");
+    setLocalForm(JSON.parse(getLocalForm));
+  }, []);
 
-  const onSubmit = () => {
+  const onSubmit = async () => {
     console.log("favorite :", favorite);
+    console.log("image :", image);
+    const getLocalForm = await localStorage.getItem("user-form");
+    const form = JSON.parse(getLocalForm);
+    const data = new FormData();
+
+    data.append("image", image);
+    data.append("email", form.email);
+    data.append("password", form.password);
+    data.append("username", form.name);
+    data.append("phoneNumber", "08123456789");
+    data.append("role", "user");
+    data.append("status", "Y");
+    data.append("favorite", favorite);
+
+    const result = setSignUp(data);
+    console.log("result : ", result);
   };
   return (
     <>
@@ -28,26 +55,40 @@ export default function SignUpPhoto() {
                 <div className="mb-20">
                   <div className="image-upload text-center">
                     <label htmlFor="avatar">
-                      <Image
-                        src="/icon/upload.svg"
-                        width={120}
-                        height={120}
-                        alt="upload"
-                      />
+                      {imagePreview ? (
+                        <img
+                          src={imagePreview}
+                          className="image-upload"
+                          alt="upload"
+                        />
+                      ) : (
+                        <Image
+                          src="/icon/upload.svg"
+                          width={120}
+                          height={120}
+                          alt="upload"
+                        />
+                      )}
                     </label>
                     <input
                       id="avatar"
                       type="file"
                       name="avatar"
                       accept="image/png, image/jpeg"
+                      onChange={(event) => {
+                        console.log(event.target.files);
+                        const image = event.target.files[0];
+                        setImagePreview(URL.createObjectURL(image));
+                        return setImage(image);
+                      }}
                     />
                   </div>
                 </div>
                 <h2 className="fw-bold text-xl text-center color-palette-1 m-0">
-                  Shayna Anne
+                  {localForm.name}
                 </h2>
                 <p className="text-lg text-center color-palette-1 m-0">
-                  shayna@anne.com
+                  {localForm.email}
                 </p>
                 <div className="pt-50 pb-50">
                   <label
@@ -65,7 +106,11 @@ export default function SignUpPhoto() {
                     onChange={(event) => setFavorite(event.target.value)}
                   >
                     {categories.map((e) => {
-                      return <option value={e._id}>{e.name}</option>;
+                      return (
+                        <option key={e._id} value={e._id}>
+                          {e.name}
+                        </option>
+                      );
                     })}
                   </select>
                 </div>
